7e52d6fcb6d01ba89bb44c12537b02fffbc22215
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-04 14:22:18 +0800

    fix: update controller gurubk

--- application/controllers/GuruBk.php
+++ application/controllers/GuruBk.php
@@ -12,25 +12,27 @@ class GuruBk extends CI_Controller
     public function index()
     {
         $data['title'] = 'Dashboard';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
 
-       
+        $data['user_role'] = $data['user']['role_id'];
+        // $data['level'] = $data['user_role']['role'];
         $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar');
+        $this->load->view('templates/sidebarbk',$data);
         $this->load->view('templates/topbar',$data);
         $this->load->view('gurubk/index', $data);
         $this->load->view('templates/footer');
     }
 
-    public function DataLaporan()
+    public function dataLaporan()
     {
         $data['title'] = 'DataLaporan';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['user_role'] = $data['user']['role_id'];
         $data['DataLaporan'] = $this->M_GuruBk->lihatdataLaporan()->result();
         $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar');
+        $this->load->view('templates/sidebarbk',$data);
         $this->load->view('templates/topbar',$data);
-        $this->load->view('gurubk/laporan', $data);
+        $this->load->view('admin/datalaporan', $data);
         $this->load->view('templates/footer');
 
     }
