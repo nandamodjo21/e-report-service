a2367ee71feee199a95752e4a722b9c9616d1f0c
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-04 14:23:04 +0800

    fix: update query data laporan

--- application/models/M_GuruBk.php
+++ application/models/M_GuruBk.php
@@ -5,11 +5,11 @@ class M_GuruBk extends CI_Model
 {
     public function lihatdataLaporan()
     {
-        return $this->db->query('SELECT a.*, b.nis, c.jenis_pelanggaran
-        FROM `t_laporan` a
-        JOIN `t_datasiswa` b
-        ON (a.nis=b.nis) 
-        JOIN `t_jenis_pelanggaran` c 
-        ON (a.id_jenis_pelanggaran=c.id_jenis_pelanggaran)');
+        return $this->db->query("SELECT l.id_laporan, l.keterangan,l.tanggal_pelanggaran,
+        CASE
+        WHEN l.status = 0 THEN 'belum diverifikasi'
+        WHEN l.status = 1 THEN 'sudah diverifikasi wali kelas'
+        WHEN l.status = 2 THEN 'sudah diverifikasi guru BK'
+        END AS sts, s.*,g.*,p.*,u.* FROM `t_laporan` l, t_datasiswa s,t_jenis_pelanggaran p, user u,t_dataguru g WHERE l.id_guru=u.id_user AND u.nama_pelapor=g.id_guru AND l.nis=s.nis AND l.id_jenis_pelanggaran=p.id_jenis_pelanggaran AND l.status=1");
     }
 }
